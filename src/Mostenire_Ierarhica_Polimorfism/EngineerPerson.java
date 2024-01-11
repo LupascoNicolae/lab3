6f9d4b27606e88e354053c659aa14963b4e28c98
LupascoNicolae <146521572+LupascoNicolae@users.noreply.github.com>
2024-01-11 21:25:59 +0200

    Update EngineerPerson.java

--- src/Mostenire_Ierarhica_Polimorfism/EngineerPerson.java
+++ src/Mostenire_Ierarhica_Polimorfism/EngineerPerson.java
@@ -1,2 +1,37 @@
 package Mostenire_Ierarhica_Polimorfism;public class EngineerPerson {
 }
+import java.util.Scanner;
+
+public class EngineerPerson extends WorkerPerson {
+    private String specialization;
+
+    public EngineerPerson(String name, int age, String profession, String specialization) {
+        super(name, age, profession);
+        this.specialization = specialization;
+    }
+
+    public EngineerPerson() {}
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    @Override
+    public void describe() {
+        System.out.println("Enter the name, age, profession and specialization delimited by enter: ");
+    }
+
+    @Override
+    public String toString() {
+        return "EngineerPerson{" +
+                "name=" + super.getName() +
+                ", age=" + super.getAge() +
+                ", profession=" + super.getProfession() +
+                ", specialization=" + this.getSpecialization() +
+                '}';
+    }
+}
