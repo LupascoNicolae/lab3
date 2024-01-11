1e8c56b9ebf6baea9e30b4825a43cef0fa3eeb8b
LupascoNicolae <146521572+LupascoNicolae@users.noreply.github.com>
2024-01-11 21:30:15 +0200

    Update SamsungSmartphone.java

--- src/Mostenire_Simpla_Abstractizare/SamsungSmartphone.java
+++ src/Mostenire_Simpla_Abstractizare/SamsungSmartphone.java
@@ -1,2 +1,47 @@
 package Mostenire_Simpla_Abstractizare;public class SamsungSmartphone {
+
+package Mostenire_Simpla_Abstractizare;
+
+public class SamsungSmartphone implements SmartDevice {
+    private int ram;
+    private double weight;
+
+    public SamsungSmartphone(int ram, double weight) {
+        this.ram = ram;
+        this.weight = weight;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    @Override
+    public void color() {
+        System.out.println("Blue");
+    }
+
+    @Override
+    public void displaySize() {
+        System.out.println("Screen size: 7cm");
+    }
+
+    @Override
+    public String toString() {
+        return "SamsungSmartphone{" +
+                "ram=" + ram +
+                ", weight=" + weight +
+                '}';
+    }
 }
