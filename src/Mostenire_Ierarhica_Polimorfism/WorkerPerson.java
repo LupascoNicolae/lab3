944e3660b4c95f27cb4efdef9c50dd7d20e57967
LupascoNicolae <146521572+LupascoNicolae@users.noreply.github.com>
2024-01-11 21:28:22 +0200

    Update WorkerPerson.java

--- src/Mostenire_Ierarhica_Polimorfism/WorkerPerson.java
+++ src/Mostenire_Ierarhica_Polimorfism/WorkerPerson.java
@@ -1,2 +1,36 @@
 package Mostenire_Ierarhica_Polimorfism;public class WorkerPerson {
+
+  import java.util.Scanner;
+
+public class WorkerPerson extends Person {
+    private String profession;
+
+    public WorkerPerson(String name, int age, String profession) {
+        super(name, age);
+        this.profession = profession;
+    }
+
+    public WorkerPerson() {}
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    @Override
+    public void describe() {
+        System.out.println("Enter the name, age and profession delimited by enter: ");
+    }
+
+    @Override
+    public String toString() {
+        return "WorkerPerson{" +
+                "name=" + super.getName() +
+                ", age=" + super.getAge() +
+                ", profession=" + this.getProfession() +
+                '}';
+    }
 }
