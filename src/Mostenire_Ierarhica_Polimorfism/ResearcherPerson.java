0f7ec1cb3879f1074927b8649dec4f6cedb81176
LupascoNicolae <146521572+LupascoNicolae@users.noreply.github.com>
2024-01-11 21:27:31 +0200

    Update ResearcherPerson.java

--- src/Mostenire_Ierarhica_Polimorfism/ResearcherPerson.java
+++ src/Mostenire_Ierarhica_Polimorfism/ResearcherPerson.java
@@ -1,2 +1,40 @@
 package Mostenire_Ierarhica_Polimorfism;public class ResearcherPerson {
+
+package Mostenire_Ierarhica_Polimorfism;
+
+import java.util.Scanner;
+
+public class ResearcherPerson extends EngineerPerson {
+    private String domain;
+
+    public ResearcherPerson(String name, int age, String profession, String specialization, String domain) {
+        super(name, age, profession, specialization);
+        this.domain = domain;
+    }
+
+    public ResearcherPerson() {}
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    @Override
+    public void describe() {
+        System.out.println("Enter the name, age, profession, specialization and domain delimited by enter: ");
+    }
+
+    @Override
+    public String toString() {
+        return "ResearcherPerson{" +
+                "name=" + super.getName() +
+                ", age=" + super.getAge() +
+                ", profession=" + super.getProfession() +
+                ", specialization=" + super.getSpecialization() +
+                ", domain=" + this.getDomain() +
+                '}';
+    }
 }
