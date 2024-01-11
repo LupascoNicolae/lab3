6a036a27777afc99cee3d78982d307408d10cd26
LupascoNicolae <146521572+LupascoNicolae@users.noreply.github.com>
2024-01-11 21:31:15 +0200

    Update Vegetation.java

--- src/Mostenire_Simpla_Abstractizare/Vegetation.java
+++ src/Mostenire_Simpla_Abstractizare/Vegetation.java
@@ -1,2 +1,49 @@
 package Mostenire_Simpla_Abstractizare;public class Vegetation {
+
+  abstract class Vegetation {
+    private int years;
+    private double height;
+    private String location;
+
+    public Vegetation(int years, double height, String location) {
+        this.years = years;
+        this.height = height;
+        this.location = location;
+    }
+
+    public void setYears(int years) {
+        this.years = years;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getYears() {
+        return this.years;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public abstract void owner();
+    public abstract int numberOfLeaves();
+
+    @Override
+    public String toString() {
+        return "Vegetation{" +
+                "years=" + this.years +
+                ", height=" + this.height +
+                ", location='" + this.location + '\'' +
+                '}';
+    }
 }
