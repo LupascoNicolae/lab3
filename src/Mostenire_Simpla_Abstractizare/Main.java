9bc4d0a1edd955e70d0af89d78d4b05be45241cf
LupascoNicolae <146521572+LupascoNicolae@users.noreply.github.com>
2024-01-11 21:29:50 +0200

    Update Main.java

--- src/Mostenire_Simpla_Abstractizare/Main.java
+++ src/Mostenire_Simpla_Abstractizare/Main.java
@@ -1,2 +1,20 @@
 package Mostenire_Simpla_Abstractizare;public class Main {
+
+public class Main {
+    public static void main(String[] args) {
+        SamsungSmartphone samsungPhone = new SamsungSmartphone(8, 150.5);
+        WalnutTree walnutTree = new WalnutTree(15, 8.5, "Backyard", "Brown");
+
+        samsungPhone.color();
+        samsungPhone.displaySize();
+        System.out.println(samsungPhone.toString());
+
+        System.out.println("-----------------------------");
+
+        System.out.println("Walnut Tree Details:");
+        System.out.println("Owner: ");
+        walnutTree.owner();
+        System.out.println("Number of Leaves: " + walnutTree.numberOfLeaves());
+        System.out.println(walnutTree.toString());
+    }
 }
